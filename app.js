c921a97f91ccad60e9e9395625cd2713fa030e4e
James Crutchley <jamescrutchley98@gmail.com>
2023-12-01 12:45:12 +1100

    Add mongoose & set up env variable

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
+require("dotenv").config();
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require("mongoose");
+
 
 var commentRouter = require('./routes/commentRoutes');
 var postRouter = require('./routes/postRoutes');
 var userRouter = require('./routes/userRoutes');
+
+const mongoDb = process.env.MONGODB_URI || process.env.DEV_URI;
+
+mongoose.connect(mongoDb);
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "mongo connection error"));
+
 
 var app = express();
 
@@ -15,7 +25,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', userRouter);
 app.use('/posts', commentRouter);
@@ -26,15 +35,19 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
+app.use(express.json());
+
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  // return error as json
+  res.status(500).json({
+    message: err.message,
+    error: err
+});
 });
 
 module.exports = app;
