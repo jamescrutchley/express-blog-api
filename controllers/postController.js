cd6464032ab377c3fd4719628b4471e77f685559
James Crutchley <jamescrutchley98@gmail.com>
2023-12-05 21:09:26 +1100

    Implement initial post and user controller methods

---
+++ controllers/postController.js
@@ -0,0 +1,135 @@
+const asyncHandler = require("express-async-handler");
+const Post = require("../models/Post");
+const User = require("../models/User");
+const mongoose = require("mongoose");
+const { body, validationResult } = require("express-validator");
+
+const postFields = ['title', 'copy', 'author', 'date', 'tags', 'comments', 'likes', 'status']
+
+exports.getPostById = asyncHandler(async (req, res, next) => {
+  const postId = req.params.postId;
+  if (!mongoose.Types.ObjectId.isValid(postId)) {
+    return res.status(400).json({
+      status: "error",
+      message: "Invalid post ID",
+    });
+  }
+  const post = await Post.findById(req.params.postId).populate("likes").exec();
+  if (post === null) {
+    // No results.
+    const err = new Error("Post not found");
+    err.status = 404;
+    return next(err);
+  }
+  res.json(post);
+});
+
+exports.getAllPosts = asyncHandler(async (req, res, next) => {
+  const posts = await Post.find({}).exec();
+
+  if (posts === null) {
+    // No results.
+    const err = new Error("No posts found");
+    err.status = 404;
+    return next(err);
+  }
+  res.json(posts);
+});
+
+exports.createPost = asyncHandler(async (req, res, next) => {
+  //REQUIRES AUTH SET UP.
+  try {
+    const post = new Post({
+      title: "test",
+      copy: "lorem ipsum",
+      author: "656939e77065f16bf01a3199",
+      date: new Date(),
+      tags: ["tag 1"],
+      comments: [],
+      likes: [],
+      status: "published",
+    });
+    const result = await post.save();
+    if (result) {
+      res.status(201).json({
+        status: "success",
+        message: "Post Published",
+        data: result,
+      });
+    } else {
+      res.status(500).json({
+        status: "error",
+        message: "Failed to create post",
+        data: post,
+      });
+    }
+  } catch (err) {
+    if (err.name === "ValidationError") {
+      // Extract validation errors
+      const validationErrors = {};
+      for (const field in err.errors) {
+        validationErrors[field] = err.errors[field].message;
+      }
+
+      res.status(422).json({
+        status: "validation_error",
+        message: "Validation failed",
+        errors: validationErrors,
+        data: post,
+      });
+    } else {
+      return next(err);
+    }
+  }
+});
+
+exports.updatePost = asyncHandler(async (req, res, next) => {
+  try {
+    const postToUpdate = await Post.findById(req.params.postId);
+    if (postToUpdate) {
+        console.log('post found');
+        //validate
+        postFields.forEach(field => {
+            if (req.body[field] !== undefined) {
+              postToUpdate[field] = req.body[field];
+            }
+          })
+          const updatedPost = await postToUpdate.save();
+          res.status(200).json({
+            status: "success",
+            message: "Post updated successfully",
+            data: updatedPost
+          });
+    } else {
+        res.status(500).json({
+          status: "error",
+          message: "Post could not be updated",
+        });
+      }
+    } catch (err) {
+      next(err);
+    }
+  });
+
+
+
+exports.deletePost = asyncHandler(async (req, res, next) => {
+    //auth
+  try {
+    const postToDelete = await Post.findByIdAndDelete(req.params.postId).exec();
+    if (postToDelete) {
+        res.status(201).json({
+          status: "success",
+          message: "Post deleted successfully",
+          data: postToDelete,
+        });
+      } else {
+        res.status(500).json({
+          status: "error",
+          message: "Post could not be deleted",
+        });
+      }
+    } catch (err) {
+      next(err);
+    }
+  });
---
+++ controllers/userController.js
@@ -0,0 +1,163 @@
+const asyncHandler = require("express-async-handler");
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const mongoose = require("mongoose");
+const { body, validationResult } = require("express-validator");
+
+exports.getHome = asyncHandler(async (req, res, next) => {
+  res.send("home");
+});
+
+// get a specific user
+exports.getUserById = asyncHandler(async (req, res, next) => {
+  const userId = req.params.userId;
+
+  if (!mongoose.Types.ObjectId.isValid(userId)) {
+    return res.status(400).json({
+      status: "error",
+      message: "Invalid user ID",
+    });
+  }
+
+  const user = await User.findById(req.params.userId, "username").exec();
+
+  if (user === null) {
+    // No results.
+    const err = new Error("User not found");
+    err.status = 404;
+    return next(err);
+  }
+
+  res.json(user);
+});
+
+exports.getAllUsers = asyncHandler(async (req, res, next) => {
+  const users = await User.find({}, "username").exec();
+
+  if (users === null) {
+    // No results.
+    const err = new Error("No users found");
+    err.status = 404;
+    return next(err);
+  }
+  res.json(users);
+});
+
+exports.createUser = asyncHandler(async (req, res, next) => {
+  //validate - check for uniqueness
+
+  bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
+    try {
+      const user = new User({
+        username: req.body.username,
+        hashedPassword: hashedPassword,
+        email: req.body.email,
+        admin: false,
+      });
+      const result = await user.save();
+      if (result) {
+        res.status(201).json({
+          status: "success",
+          message: "User created successfully",
+          data: {
+            username: result.username,
+            email: result.email,
+            admin: result.admin,
+        }
+        });
+      } else {
+        res.status(500).json({
+          status: "error",
+          message: "Failed to create user",
+        });
+      }
+    } catch (err) {
+      if (err.name === "ValidationError") {
+        // Extract validation errors
+        const validationErrors = {};
+        for (const field in err.errors) {
+          validationErrors[field] = err.errors[field].message;
+        }
+
+        res.status(422).json({
+          status: "validation_error",
+          message: "Validation failed",
+          errors: validationErrors,
+          data: {
+            username: req.body.username,
+            password: req.body.password,
+            email: req.body.email,
+          },
+        });
+      } else {
+        return next(err);
+      }
+    }
+  });
+});
+
+exports.updateUser = asyncHandler(async (req, res, next) => {
+  try {
+    const userToUpdate = await User.findById(req.params.userId);
+    //if it exists, validate fields:
+    if (userToUpdate) {
+        console.log('user found')
+      //validate...validate
+      //if validation fails send back validation errors and bounce back client input data just as for createUser.
+
+      if (req.body.username) {
+        userToUpdate.username = req.body.username;
+      }
+      if (req.body.email) {
+        userToUpdate.email = req.body.email;
+      }
+      if (req.body.admin) {
+        userToUpdate.admin = req.body.admin;
+      }
+      if (req.body.password) {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        userToUpdate.password = hashedPassword;
+      }
+
+      const updatedUser = await userToUpdate.save();
+      res.status(200).json({
+        status: "success",
+        message: "User updated successfully",
+        data: {
+            username: updatedUser.username,
+            email: updatedUser.email,
+            admin: updatedUser.admin,
+        }
+      });
+    } else {
+      res.status(500).json({
+        status: "error",
+        message: "User could not be updated",
+      });
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
+exports.deleteUser = asyncHandler(async (req, res, next) => {
+  //authentication
+  //...
+  try {
+    const userToDelete = await User.findByIdAndDelete(req.params.userId).exec();
+    if (userToDelete) {
+      res.status(201).json({
+        status: "success",
+        message: "User deleted successfully",
+        data: userToDelete,
+      });
+    } else {
+      res.status(500).json({
+        status: "error",
+        message: "User could not be deleted",
+      });
+    }
+  } catch (err) {
+    next(err);
+  }
+});
