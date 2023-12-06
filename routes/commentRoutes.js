3b685436d42f9849d63f621b827d5844f7982345
James Crutchley <jamescrutchley98@gmail.com>
2023-12-06 13:32:10 +1100

    Fix comment route

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -5,7 +5,7 @@ const commentController = require('../controllers/commentController');
 
 router.get('/:postId/comments/:commentId', commentController.getCommentById);
 router.get('/:postId/comments', commentController.getAllComments)
-router.post('/:postId/comments/:commentId', commentController.createComment)
+router.post('/:postId/comments', commentController.createComment)
 router.put('/:postId/comments/:commentId', commentController.updateComment)
 router.delete('/:postId/comments/:commentId', commentController.deleteComment)
 
