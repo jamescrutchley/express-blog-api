3806eebd2db828b5171e7b1687ed695afb123b99
James Crutchley <jamescrutchley98@gmail.com>
2023-12-13 15:02:08 +1100

    Update auth middleware

--- middleware/jwt-auth.js
+++ middleware/jwt-auth.js
@@ -3,25 +3,39 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
+
+//auth middleware logic:
+// no token - mark req.user as null
+// invalid/expired token - mark req.user as null
+// valid token - set user fields
+// proceed
+
 function authenticateToken(req, res, next) {
-    const authHeader = req.headers['authorization']
-    const token = authHeader && authHeader.split(' ')[1]
-  
-    if (token == null) {
-        req.user = null;
-    }
-  
-    jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
-      if (err) return res.sendStatus(403);
-    
-      req.user = {
-        userId: user.sub,
-        username: user.username,
-        isAdmin: user.admin || false, // Assuming 'admin' is a property in the user object
-      };
-    
-      next();
-    });
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (token == null) {
+    req.user = null;
   }
 
-  module.exports = authenticateToken;
+  // if (token == null || req.user === null) {
+  //     return next(); // Proceed to the next middleware without attempting to verify the token
+  // }
+
+  jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
+    if (err) {
+      req.user = null; // Set req.user to null if there's an error during token verification
+      return next(); // Proceed to the next middleware without triggering an error response
+    }
+
+    req.user = {
+      userId: user.sub,
+      username: user.username,
+      isAdmin: user.admin || false, 
+    };
+
+    next();
+  });
+}
+
+module.exports = authenticateToken;
