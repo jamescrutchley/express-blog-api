f2899304fcbc6ea72c9bc8e646d10f332657520e
James Crutchley <jamescrutchley98@gmail.com>
2023-12-13 15:02:41 +1100

    Update isUnique function

--- validation/isUnique.js
+++ validation/isUnique.js
@@ -1,40 +1,37 @@
 const mongoose = require("mongoose");
 
- const isUnique = async (Model, values) => {
-    const orConditions = values.map(([field, value]) => ({ [field]: value }));
-    const query = { $or: orConditions };
-  
-    try {
-      const existingDocuments = await Model.find(query);
-  
-      //todo - clean this up
-      if (existingDocuments.length > 0) {
-        const conflictFields = values.reduce((acc, [field, value]) => {
-            if (existingDocuments.some(doc => doc[field] === value)) {
-              acc.push({ [field]: value });
-            }
-            return acc;
-          }, []);        
-          
-        const conflicts = conflictFields.map(field => Object.keys(field)).flat()
+const isUnique = async (Model, values) => {
+  const orConditions = values.map(([field, value]) => ({ [field]: value }));
+  const query = { $or: orConditions };
 
+  try {
+    const existingDocuments = await Model.find(query);
 
-        console.log(conflicts)
+    //todo - clean this up
+    if (existingDocuments.length > 0) {
+      const conflictFields = values.reduce((acc, [field, value]) => {
+        if (existingDocuments.some((doc) => doc[field] === value)) {
+          acc.push({ [field]: value });
+        }
+        return acc;
+      }, []);
 
-  //fix this
-        return {
-          isUnique: false,
-          conflicts: conflicts,
-          message: `Values already in use.`,
-        };
-      } else {
-        return { isUnique: true };
-      }
-    } catch (error) {
-      console.error('Error checking uniqueness:', error);
-      throw error;
+      const conflicts = conflictFields
+        .map((field) => Object.keys(field))
+        .flat();
+
+      return {
+        isUnique: false,
+        conflicts: conflicts,
+        message: `Values already in use.`,
+        existingDocument: existingDocuments[0]
+      };
+    } else {
+      return { isUnique: true };
     }
-  };
-  
+  } catch (error) {
+    throw error;
+  }
+};
 
 module.exports = isUnique;
